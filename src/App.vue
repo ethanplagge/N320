eb762b54ced0addc923de7c5502bd5dea44c9e5d
ethanplagge <112018558+ethanplagge@users.noreply.github.com>
2023-11-28 12:06:56 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <app-header @submit-data="createTeam">
+    
+    
+  </app-header>
+  <team-info :parentTeamName="teams" @join-data="addMember"></team-info>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return {
+      showContent: true,
+      teams: []
+      
+    }
+  },
+  methods: {
+    createTeam(name, leadRole){
+      const newTeam = {
+        teamName: name,
+        leadRole: leadRole,
+        developer: '',
+        UIUX: '',
+        designer: ''
+      }
+      this.teams.push(newTeam);
+
+      console.log(this.teams);
+    },
+    addMember(name, role){
+      if(role === 'Developer' && this.teams[0].developer == ''){
+        this.teams[0].developer = name;
+      } else if(role === 'UI/UX' && this.teams[0].UIUX == ''){
+        this.teams[0].UIUX = name;
+      } else if(role === 'Designer' && this.teams[0].designer == ''){
+        this.teams[0].designer = name;
+      } else {
+        alert("already selected")
+      }
+      console.log(this.teams)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  padding: 30px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header-wrapper">
+    <form @submit.prevent="handleSubmit">
+      <h1 >Enter your Team Name Below</h1>
+    <input type="text" placeholder="Team Name" v-model="enteredName">
+    <br>
+    <div class="role-wrapper">
+      <h3 >Please Select your role.</h3>
+      <select v-model="enteredRole">
+        <option value="Developer">Developer</option>
+        <option value="Designer">Designer</option>
+        <option value="UI/UX">UI/UX</option>
+      </select>
+      <br> <br>
+    </div>
+    <button type="submit">Submit Info</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        enteredName: '',
+        enteredRole: '',
+      }
+    },
+    emits: ['submit-data'],
+    methods: {
+      handleSubmit(){
+        this.$emit('submit-data', this.enteredName, this.enteredRole)
+      },
+    },
+  }
+</script>
+
+<style>
+  
+  .role-wrapper{
+    display: block;
+  }
+</style>
